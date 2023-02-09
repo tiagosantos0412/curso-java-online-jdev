628f194ff489a3c8e17222640794c95d4efc2406
Tiago Felipe <tiagosantos0412@gmail.com>
2023-02-09 10:27:32 -0300

    Criação do projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,8 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	public static void main(String[] args) {
+		System.out.println("Olá, mundo!");
+		System.out.println("Meu primeiro programa em Java - do curso Formação Java Web");
+	}
+}
