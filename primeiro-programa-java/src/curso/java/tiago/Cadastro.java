84f47b097db10b5ca9fd15337719d3bf96f32f02
Tiago Felipe <tiagosantos0412@gmail.com>
2023-02-09 11:53:14 -0300

    Adicionando o atributo CPF na classe Pessoa.

--- primeiro-programa-java/src/curso/java/tiago/Cadastro.java
+++ primeiro-programa-java/src/curso/java/tiago/Cadastro.java
@@ -4,17 +4,15 @@ public class Cadastro {
 
 	public static void main(String[] args) {
 		
-		String nome = "Tiago";
-		String sobreNome = "Felipe dos Santos";
-		int idade = 36;
-		
 		Pessoa p1 = new Pessoa();
 		
-		p1.setNome(nome);
-		p1.setSobrenome(sobreNome);
-		p1.setIdade(idade);
+		p1.setNome("Tiago");
+		p1.setSobrenome("Felipe dos Santos");
+		p1.setIdade(36);
+		p1.setCpf("36568593885");
 		
 		System.out.println(p1.getNome() + " " + p1.getSobrenome() + " " + p1.getIdade() + " anos.");
+		System.out.println("Número do CPF: " + p1.getCpf());
 
 	}
 
--- primeiro-programa-java/src/curso/java/tiago/Pessoa.java
+++ primeiro-programa-java/src/curso/java/tiago/Pessoa.java
@@ -5,7 +5,14 @@ public class Pessoa {
 	private String nome;
 	private String sobrenome;
 	private int idade;
+	private String cpf;
 	
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
 	public String getNome() {
 		return nome;
 	}
