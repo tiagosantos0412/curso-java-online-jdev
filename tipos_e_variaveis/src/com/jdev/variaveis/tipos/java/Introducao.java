486513160d1b08c054ec2fa525cc210040c15ee4
Tiago Felipe <tiagosantos0412@gmail.com>
2023-02-10 09:09:50 -0300

    adicionando o projeto de aulas de tipos e variaveis.

---
+++ tipos_e_variaveis/src/com/jdev/variaveis/tipos/java/Introducao.java
@@ -0,0 +1,24 @@
+package com.jdev.variaveis.tipos.java;
+
+public class Introducao {
+
+	public static void main(String[] args) {
+		/*
+		 * Tipos: Entendemos por tipos de informação
+		 * 
+		 * Variável: Uma variável referenciará o tipo de dado
+		 * 
+		 * */
+		
+		byte tipoByte;
+		short tipoShort;
+		int tipoInt;
+		boolean tipoBoolean;
+		String tipoTexto;
+		float tipoFloatf;
+		double tipoDouble;
+		char tipoCaractere;
+
+	}
+
+}
